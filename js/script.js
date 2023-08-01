b50846746fef65e3f5b42fc0dad5c9516a23ac7c
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2023-08-01 19:40:54 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+var moodSelect = document.querySelector(".mood-select");
+moodSelect.addEventListener("change", function (e) {
+   var mood = e.target.value
+     if (mood === "happy") {
+        body.classList.remove("sad")
+        body.classList.remove("passionate");
+        body.classList.add("happy");
+     }  
+     else if (mood === "sad") {
+        body.classList.remove("happy")
+        body.classList.remove("passionate");
+        body.classList.add("sad");
+     }
+     else if (mood === "passionate") {
+        body.classList.remove("sad");
+        body.classList.remove("happy");
+        body.classList.add("passionate");
+     }
+});
+
+var body = document.querySelector("body");
